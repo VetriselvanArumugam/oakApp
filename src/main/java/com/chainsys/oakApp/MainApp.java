aadc5521dcd35ca3436b351e8540a2af61f8faf6
Vetri45 <vetriselvan1045@gmail.com>
2022-07-25 15:36:49 +0530

    add

--- src/main/java/com/chainsys/oakApp/MainApp.java
+++ src/main/java/com/chainsys/oakApp/MainApp.java
@@ -3,6 +3,7 @@ package com.chainsys.oakApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring
 @SpringBootApplication
 public class MainApp {
 
